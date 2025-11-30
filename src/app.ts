abe18c69df3a934b7ae92c43d4da2011d6f1e377
AjaySusanth <ajaysusanth10@gmail.com>
2025-11-30 09:33:25 +0530

    feat: implemented middleware(#3)

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import healthRoutes from './routes/health.js'
 import authRoutes from './modules/auth/auth.route.js'
-
+import userRoutes from "./modules/user/user.route.js";
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
 // API routes
-app.use('/api', healthRoutes);
-app.use('/api', authRoutes);
+app.use("/api", healthRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 export default app;
+
+
+
